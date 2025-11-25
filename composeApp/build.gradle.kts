58ee22ca0fb0d21ab968afff0d6fe2806014e53c
david <davidlukash@hotmail.com>
2025-11-25 23:28:09 +0000

    Added compose navigation library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.compose.navigaion)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
