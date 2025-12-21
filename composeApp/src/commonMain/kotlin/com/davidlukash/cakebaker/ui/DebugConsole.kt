bad830ac3456112cc3c74426b9c5da9c98250a65
david <davidlukash@hotmail.com>
2025-12-21 11:15:05 +0000

    Made debug console auto scroll

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/DebugConsole.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/DebugConsole.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.selection.SelectionContainer
@@ -20,14 +18,13 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.SpanStyle
@@ -67,10 +64,14 @@ data class Log @OptIn(ExperimentalUuidApi::class, ExperimentalTime::class) const
 fun RowScope.DebugConsole() {
     val mainViewModel = LocalMainViewModel.current
     val engine = mainViewModel.engine
-    val dataViewModel = mainViewModel.dataViewModel
     var logs by remember { mutableStateOf(listOf<Log>()) }
     var input by remember { mutableStateOf("") }
-    Expression
+    val lazyListState = rememberLazyListState()
+
+    LaunchedEffect(logs) {
+        lazyListState.scrollToItem(logs.size - 1)
+    }
+
     Column(
         modifier = Modifier.weight(1f).background(color = background).fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
@@ -89,6 +90,7 @@ fun RowScope.DebugConsole() {
             LazyColumn(
                 modifier = Modifier.fillMaxSize().padding(8.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
+                state = lazyListState
             ) {
                 items(
                     logs.size,
