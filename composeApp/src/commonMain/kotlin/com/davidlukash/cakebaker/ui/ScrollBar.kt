341d2dbb01c4ca877be2fbf71eb48f0acfc99480
david <davidlukash@hotmail.com>
2026-01-01 20:33:13 +0000

    Updated scroll bar to be more accurate

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/ScrollBar.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/ScrollBar.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import com.davidlukash.cakebaker.viewmodel.LocalMainViewModel
@@ -27,6 +30,12 @@ fun HorizontalScrollBar(scrollState: ScrollState, coroutineScope: CoroutineScope
     val mainViewModel = LocalMainViewModel.current
     val density = LocalDensity.current
     val viewportWidth = density.run { scrollState.viewportSize.toDp() }
+    val maxValue = density.run { scrollState.maxValue.toDp() }
+    var scrollBarWidth by remember { mutableStateOf(0.dp) }
+    val value = density.run { scrollState.value.toDp() }
+    val visibleFraction = viewportWidth / maxValue
+    val fraction = (viewportWidth - scrollBarWidth) / maxValue
+
     val themeViewModel = mainViewModel.themeViewModel
     val theme by themeViewModel.theme.collectAsState()
     if (scrollState.canScrollForward || scrollState.canScrollBackward) {
@@ -36,14 +45,17 @@ fun HorizontalScrollBar(scrollState: ScrollState, coroutineScope: CoroutineScope
         ) {
             Surface(
                 color = theme.buttonTheme.containerColor,
-                modifier = Modifier.width(viewportWidth - scrollState.maxValue.dp).height(32.dp)
-                    .absoluteOffset(x = scrollState.value.dp).pointerInput(Unit) {
+                modifier = Modifier.fillMaxWidth(visibleFraction).height(32.dp)
+                    .absoluteOffset(x = value * fraction)
+                    .pointerInput(Unit) {
                         detectHorizontalDragGestures { change, dragAmount ->
                             coroutineScope.launch {
-                                scrollState.scrollBy(dragAmount)
+                                scrollState.scrollBy(dragAmount / fraction)
                             }
                         }
-                    }.border(6.dp, theme.buttonTheme.borderColor)
+                    }.border(6.dp, theme.buttonTheme.borderColor).onPlaced {
+                        scrollBarWidth = density.run { it.size.width.toDp() }
+                    }
             ) {
 
             }
