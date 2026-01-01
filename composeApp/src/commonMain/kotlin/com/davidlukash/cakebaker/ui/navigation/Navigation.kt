7785905795fc554e2f8b6abf0d802fcbb582d43f
david <davidlukash@hotmail.com>
2026-01-01 14:24:57 +0000

    Refactored Navigation.kt

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/navigation/Navigation.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/navigation/Navigation.kt
@@ -1,7 +1,5 @@
 package com.davidlukash.cakebaker.ui.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
