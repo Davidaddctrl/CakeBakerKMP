4cc2815b9d767f9c41449ddc117fed5e11692b6a
david <davidlukash@hotmail.com>
2026-01-01 13:19:01 +0000

    Separated ItemTopRow to a separate composable to improve re-usability and add scroll bar

---
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/ItemTopRow.kt
@@ -0,0 +1,49 @@
+package com.davidlukash.cakebaker.ui
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.davidlukash.cakebaker.horizontalRowScroll
+import com.davidlukash.cakebaker.viewmodel.LocalMainViewModel
+
+@Composable
+fun ItemTopRow() {
+    val mainViewModel = LocalMainViewModel.current
+    val dataViewModel = mainViewModel.dataViewModel
+    val items by dataViewModel.allItemsFlow.collectAsState(initial = emptyList())
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier.horizontalScroll(scrollState)
+                .horizontalRowScroll(coroutineScope, scrollState).fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
+        ) {
+            items.forEach { item ->
+                key(item.name) {
+                    ItemAmountDisplay(
+                        item = item,
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        HorizontalScrollBar(scrollState, coroutineScope)
+    }
+}
