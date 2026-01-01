d9e692168de43a4c9da3e95c2c0f9faeb085e97e
david <davidlukash@hotmail.com>
2026-01-01 13:29:09 +0000

    Wrote Cake Baker engine and scope

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/CakeBakerEngine.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/CakeBakerEngine.kt
@@ -1,176 +1,19 @@
 package com.davidlukash.cakebaker.viewmodel
 
-import com.davidlukash.cakebaker.data.ItemType
-import com.davidlukash.cakebaker.toInt
-import com.davidlukash.jsonmath.BasicOperatorsEngine
-import com.davidlukash.jsonmath.Operation
-import com.ionspin.kotlin.bignum.decimal.BigDecimal
+import com.davidlukash.cakebaker.globalDecimalMode
+import com.davidlukash.jsonmath.engine.normal.Engine
+import com.davidlukash.jsonmath.engine.normal.ScopeType
 import com.ionspin.kotlin.bignum.decimal.DecimalMode
 import com.ionspin.kotlin.bignum.decimal.RoundingMode
-import com.ionspin.kotlin.bignum.decimal.toBigDecimal
 
 class CakeBakerEngine(
     val mainViewModel: MainViewModel
-) : BasicOperatorsEngine(
+) : Engine<CakeBakerScope>(
     decimalMode = DecimalMode(
-        decimalPrecision = 0,
+        decimalPrecision = 200L,
         roundingMode = RoundingMode.FLOOR,
-        scale = 2
+        scale = 10L
     )
 ) {
-    override fun getReference(reference: String): BigDecimal {
-        val parts = reference.split(".")
-        val first = parts[0]
-        val dataViewModel = mainViewModel.dataViewModel
-        return when (first) {
-            "items" -> {
-                val itemName = parts[1]
-                val item = dataViewModel.allItemsFlow.value.find { it.name == itemName }
-                    ?: throw NoSuchElementException("$reference not found.")
-                val third = parts[2]
-                when (third) {
-                    "type" -> ItemType.entries.indexOf(item.type).toBigDecimal(decimalMode = decimalMode)
-                    "amount" -> item.amount
-                    "price" -> item.price
-                    "fastPriceGrowth" -> item.fastPriceGrowth?.toInt()?.toBigDecimal(decimalMode = decimalMode)
-                    "total" -> item.total
-                    "increment" -> item.increment
-                    "increaseSlope" -> item.increaseSlope
-                    "cakePriceAccountability" -> {
-                        val index = parts[3]
-                        item.cakePriceAccountability?.get(index.toInt())
-                    }
-
-                    "cakePrices" -> {
-                        val index = parts[3]
-                        item.cakePrices?.get(index.toInt())
-                    }
-
-                    "cakeTier" -> item.cakeTier?.toBigDecimal(decimalMode = decimalMode)
-                    "salePrice" -> item.salePrice
-                    else -> throw NoSuchElementException("$reference not found.")
-                } ?: BigDecimal.parseString("-1")
-            }
-
-            "orderCakeSettings" -> {
-                val tier = parts[1].toInt()
-                val settings =
-                    dataViewModel.orderCakeSettings.value[tier]
-                        ?: throw NoSuchElementException("$reference not found.")
-                val third = parts[2]
-                when (third) {
-                    "allocatedTimeMax" -> settings.allocatedTimeMax.toBigDecimal()
-                    "allocatedTimeMin" -> settings.allocatedTimeMin.toBigDecimal()
-                    "saleMaxChange" -> settings.saleMaxChange.toBigDecimal()
-                    "saleMinChange" -> settings.saleMinChange.toBigDecimal()
-                    "maxAmount" -> settings.maxAmount.toBigDecimal()
-                    "waitTimeMax" -> settings.waitTimeMax.toBigDecimal()
-                    "waitTimeMin" -> settings.waitTimeMin.toBigDecimal()
-                    "maxFulfilledCustomerSatisfaction" -> settings.maxFulfilledCustomerSatisfaction.toBigDecimal()
-                    "minFulfilledCustomerSatisfaction" -> settings.minFulfilledCustomerSatisfaction.toBigDecimal()
-                    "maxUnfulfilledCustomerSatisfaction" -> settings.maxUnfulfilledCustomerSatisfaction.toBigDecimal()
-                    "minUnfulfilledCustomerSatisfaction" -> settings.minUnfulfilledCustomerSatisfaction.toBigDecimal()
-                    else -> throw NoSuchElementException("$reference not found.")
-                }
-            }
-
-            else -> throw NoSuchElementException("$reference not found.")
-        }
-    }
-
-    override fun setReference(reference: String, value: BigDecimal) {
-        val parts = reference.split(".")
-        val first = parts[0]
-        val dataViewModel = mainViewModel.dataViewModel
-        when (first) {
-            "items" -> {
-                val itemName = parts[1]
-                val item = dataViewModel.allItemsFlow.value.find { it.name == itemName }
-                if (item == null) {
-                    throw NoSuchElementException("$reference not found.")
-                }
-                val third = parts[2]
-                dataViewModel.updateItem(
-                    when (third) {
-                        "type" -> item.copy(
-                            type = ItemType.entries[value.intValue(false)]
-                        )
-
-                        "amount" -> item.copy(amount = value)
-                        "price" -> item.copy(price = value)
-                        "fastPriceGrowth" -> item.copy(fastPriceGrowth = value > BigDecimal.ZERO)
-                        "total" -> item.copy(total = value)
-                        "increment" -> item.copy(increment = value)
-                        "increaseSlope" -> item.copy(increaseSlope = value)
-                        "cakePriceAccountability" -> {
-                            val index = parts[3].toInt()
-                            item.copy(
-                                cakePriceAccountability = (item.cakePriceAccountability ?: mapOf()) + (index to value)
-                            )
-                        }
-
-                        "cakePrices" -> {
-                            val index = parts[3].toInt()
-                            item.copy(
-                                cakePrices = (item.cakePrices ?: mapOf()) + (index to value)
-                            )
-                        }
-
-                        "cakeTier" -> item.copy(cakeTier = value.intValue(false))
-                        "salePrice" -> item.copy(salePrice = value)
-                        else -> throw NoSuchElementException("$reference not found.")
-                    }
-                )
-            }
-
-            "orderCakeSettings" -> {
-                val tier = parts[1].toInt()
-                val settings =
-                    dataViewModel.orderCakeSettings.value[tier]
-                        ?: throw NoSuchElementException("$reference not found.")
-                val third = parts[2]
-                dataViewModel.updateOrderSettings(
-                    tier,
-                    when (third) {
-                        "allocatedTimeMax" -> settings.copy(
-                            allocatedTimeMax = value.doubleValue(false)
-                        )
-                        "allocatedTimeMin" -> settings.copy(
-                            allocatedTimeMin = value.doubleValue(false)
-                        )
-                        "saleMaxChange" -> settings.copy(
-                            saleMaxChange = value.doubleValue(false)
-                        )
-                        "saleMinChange" -> settings.copy(
-                            saleMinChange = value.doubleValue(false)
-                        )
-                        "maxAmount" -> settings.copy(
-                            maxAmount = value.intValue(false)
-                        )
-                        "waitTimeMax" -> settings.copy(
-                            waitTimeMax = value.doubleValue(false)
-                        )
-                        "waitTimeMin" -> settings.copy(
-                            waitTimeMin = value.doubleValue(false)
-                        )
-                        "maxFulfilledCustomerSatisfaction" -> settings.copy(
-                            maxFulfilledCustomerSatisfaction = value.intValue(false)
-                        )
-                        "minFulfilledCustomerSatisfaction" -> settings.copy(
-                            minFulfilledCustomerSatisfaction = value.intValue(false)
-                        )
-                        "maxUnfulfilledCustomerSatisfaction" -> settings.copy(
-                            maxUnfulfilledCustomerSatisfaction = value.intValue(false)
-                        )
-                        "minUnfulfilledCustomerSatisfaction" -> settings.copy(
-                            minUnfulfilledCustomerSatisfaction = value.intValue(false)
-                        )
-                        else -> throw NoSuchElementException("$reference not found.")
-                    }
-                )
-            }
-
-            else -> throw NoSuchElementException("$reference not found.")
-        }
-    }
+    override fun createScope(scopeType: ScopeType): CakeBakerScope = CakeBakerScope(scopeType, mainViewModel.dataViewModel)
 }
---
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/CakeBakerScope.kt
@@ -0,0 +1,238 @@
+package com.davidlukash.cakebaker.viewmodel
+
+import com.davidlukash.cakebaker.data.Item
+import com.davidlukash.cakebaker.data.Upgrade
+import com.davidlukash.jsonmath.createObject
+import com.davidlukash.jsonmath.data.Object
+import com.davidlukash.jsonmath.data.ObjectType
+import com.davidlukash.jsonmath.engine.normal.*
+
+class CakeBakerScope(
+    scopeType: ScopeType,
+    val dataViewModel: DataViewModel
+) : Scope(scopeType) {
+    val localVariables = mutableMapOf<String, Object>()
+
+    override fun listVariables(): List<VariableDescriptor> {
+        return localVariables.keys.map { key ->
+            VariableDescriptor(
+                name = "locals.$key",
+                set = { localVariables[key] = it },
+            ) { localVariables[key]!! }
+        } + if (scopeType == ScopeType(EnumScopeType.GLOBAL)) {
+            dataViewModel.upgradesFlow.value.flatMap { upgrade ->
+                val directDescriptors = upgrade.toObject().asDictionary()!!.flatMap { (key, value) ->
+                    val keyString = key.asString()!!
+                    listOf(
+                        VariableDescriptor(
+                            name = "globals.upgrades.${upgrade.name}.$keyString",
+                            expectedType = when (keyString) {
+                                "pageName" -> ObjectType.STRING
+                                "imageName" -> ObjectType.STRING
+                                "name" -> ObjectType.STRING
+                                "price" -> ObjectType.NUMBER
+                                "cakeTier" -> ObjectType.NUMBER
+                                "maxLevel" -> ObjectType.NUMBER
+                                "onBuy" -> ObjectType.LIST
+                                "level" -> ObjectType.NUMBER
+                                "parameters" -> ObjectType.DICTIONARY
+                                else -> null
+                            },
+                            expectedTypeNullable = keyString == "maxLevel",
+                            set = {
+                                dataViewModel.updateUpgrade(
+                                    upgrade.mergeWith(
+                                        createObject(
+                                            mapOf(
+                                                key to it
+                                            )
+                                        )
+                                    )
+                                )
+                            }
+                        ) { value }
+                    ) + if (keyString == "parameters") {
+                        value.asDictionary()!!.map { (paramKey, paramValue) ->
+                            val paramKeyString = paramKey.asString()!!
+                            VariableDescriptor(
+                                name = "globals.upgrades.${upgrade.name}.parameters.$paramKeyString",
+                                set = { paramNewValue ->
+                                    dataViewModel.updateUpgrade(
+                                        upgrade.copy(
+                                            parameters = upgrade.parameters + mapOf(
+                                                paramKeyString to paramNewValue
+                                            )
+                                        )
+                                    )
+                                },
+                            ) { paramValue }
+                        }
+                    } else listOf()
+                }
+                listOf(
+                    VariableDescriptor(
+                        name = "globals.upgrades.${upgrade.name}",
+                        expectedType = ObjectType.DICTIONARY,
+                        expectedTypeNullable = false,
+                        set = {
+                            dataViewModel.updateUpgrade(
+                                upgrade.mergeWith(it)
+                            )
+                        }
+                    ) {
+                        upgrade.toObject()
+                    }
+                ) + directDescriptors
+            } + dataViewModel.allItemsFlow.value.flatMap { item ->
+                val directDescriptors = item.toObject().asDictionary()!!.map { (key, value) ->
+                    val keyString = key.asString()!!
+                    VariableDescriptor(
+                        name = "globals.items.${item.name}.$keyString",
+                        expectedType = when (keyString) {
+                            "name" -> ObjectType.STRING
+                            "type" -> ObjectType.STRING
+                            "amount" -> ObjectType.NUMBER
+                            "price" -> ObjectType.NUMBER
+                            "fastPriceGrowth" -> ObjectType.BOOLEAN
+                            "total" -> ObjectType.NUMBER
+                            "increment" -> ObjectType.NUMBER
+                            "increaseSlope" -> ObjectType.NUMBER
+                            "cakePriceAccountability" -> ObjectType.DICTIONARY
+                            "cakePrices" -> ObjectType.DICTIONARY
+                            "cakeTier" -> ObjectType.NUMBER
+                            "salePrice" -> ObjectType.NUMBER
+                            else -> ObjectType.NUMBER
+                        },
+                        expectedTypeNullable = keyString in listOf(
+                            "price",
+                            "fastPriceGrowth",
+                            "total",
+                            "increment",
+                            "increaseSlope",
+                            "cakePriceAccountability",
+                            "cakePrices",
+                            "cakeTier",
+                            "salePrice"
+                        ),
+                        set = {
+                            dataViewModel.updateItem(
+                                item.mergeWith(
+                                    createObject(
+                                        mapOf(
+                                            key to it
+                                        )
+                                    )
+                                )
+                            )
+                        }
+                    ) {
+                        value
+                    }
+                }
+                listOf(
+                    VariableDescriptor(
+                        name = "globals.items.${item.name}",
+                        expectedType = ObjectType.DICTIONARY,
+                        expectedTypeNullable = false,
+                        set = {
+                            dataViewModel.updateItem(
+                                item.mergeWith(it)
+                            )
+                        }
+                    ) {
+                        item.toObject()
+                    }
+                ) + directDescriptors
+            } + VariableDescriptor(
+                "globals.upgrades",
+                expectedType = ObjectType.LIST,
+                expectedTypeNullable = false,
+                set = {
+                    dataViewModel.setUpgrades(
+                        it.asList()!!.map { innerUpgrade ->
+                            Upgrade.fromObject(innerUpgrade)
+                        }
+                    )
+                }
+            ) {
+                createObject(dataViewModel.upgradesFlow.value.map { it.toObject() })
+            } + VariableDescriptor(
+                "globals.items",
+                expectedType = ObjectType.LIST,
+                expectedTypeNullable = false,
+                set = {
+                    dataViewModel.setItems(
+                        it.asList()!!.map { innerItem ->
+                            Item.fromObject(innerItem)
+                        }
+                    )
+                }
+            ) {
+                createObject(dataViewModel.allItemsFlow.value.map { it.toObject() })
+            }
+        } else listOf()
+    }
+
+    override fun addVariable(
+        name: String,
+        value: Object,
+    ) {
+        val parts = name.split(".")
+        if (parts.isEmpty()) throw createVariableNameInvalidException(name)
+        val first = parts[0]
+        when (first) {
+            "locals" -> {
+                if (scopeType.type == EnumScopeType.GLOBAL) throw LanguageException(
+                    "Cannot set a local variable in a global scope",
+                    "WrongScopeException",
+                )
+                if (parts.size != 2) throw createVariableNameInvalidException(name)
+                val second = parts[1]
+                localVariables[second] = value
+            }
+
+            "globals" -> {
+                if (scopeType.type == EnumScopeType.LOCAL) throw LanguageException(
+                    "Cannot set a global variable in a local scope",
+                    "WrongScopeException",
+                )
+                val second =
+                    parts.getOrNull(1) ?: throw createVariableNameInvalidException(name) // eg. upgrades
+                when (second) {
+                    "upgrades" -> {
+                        val upgrades = dataViewModel.upgradesFlow.value
+                        val third = parts.getOrNull(2) ?: throw createVariableNameInvalidException(
+                            name,
+                        ) //eg. Faster Oven
+                        val upgrade = upgrades.find { it.name == third } ?: throw createVariableNameInvalidException(
+                            name,
+                        )
+                        val fourth = parts.getOrNull(3) ?: throw createVariableNameInvalidException(
+                            name,
+                        ) //eg. parameters
+                        val fifth = parts.getOrNull(4) ?: throw createVariableNameInvalidException(
+                            name,
+                        ) //eg. buysUntilIncrement
+
+                        dataViewModel.updateUpgrade(
+                            upgrade.copy(
+                                parameters = upgrade.parameters + mapOf(
+                                    fifth to value
+                                )
+                            )
+                        )
+                    }
+                }
+            }
+
+            else -> throw createVariableNameInvalidException(name)
+        }
+    }
+
+    private fun createVariableNameInvalidException(name: String): LanguageException =
+        LanguageException(
+            "Invalid variable name $name",
+            "VariableNameInvalidException",
+        )
+
+}
