60a2e83b3550738ef941fe4364a5be5120e780a2
david <davidlukash@hotmail.com>
2026-01-01 02:39:07 +0000

    Added SaveScreen and made screens external objects from Screen

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/navigation/Screens.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/navigation/Screens.kt
@@ -1,44 +1,51 @@
 package com.davidlukash.cakebaker.ui.navigation
 
-import com.davidlukash.cakebaker.ui.navigation.Screen.Cake
-import com.davidlukash.cakebaker.ui.navigation.Screen.Fade
-import com.davidlukash.cakebaker.ui.navigation.Screen.Ingredient
-import com.davidlukash.cakebaker.ui.navigation.Screen.Kitchen
-import com.davidlukash.cakebaker.ui.navigation.Screen.Menu
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonTransformingSerializer
 import kotlinx.serialization.json.encodeToJsonElement
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 
 @Serializable
-sealed class Screen {
-    @Serializable
-    object Cake : Screen()
+sealed class Screen(val name: String)
 
-    @Serializable
-    object Menu : Screen()
+@Serializable
+object CakeScreen : Screen("CakeScreen")
 
-    @Serializable
-    object Fade : Screen()
+@Serializable
+object MenuScreen : Screen("MenuScreen")
 
-    @Serializable
-    object Kitchen : Screen()
+@Serializable
+object FadeScreen : Screen("FadeScreen")
 
-    @Serializable
-    object Ingredient : Screen()
+@Serializable
+object KitchenScreen : Screen("KitchenScreen")
 
-    @Serializable
-    object Upgrade : Screen()
-}
+@Serializable
+object IngredientScreen : Screen("IngredientScreen")
+
+@Serializable
+object UpgradeScreen : Screen("UpgradeScreen")
+
+@Serializable
+object SaveScreen : Screen("SaveScreen")
 
 val allScreens = listOf(
-    Cake,
-    Menu,
-    Fade,
-    Kitchen,
-    Ingredient,
-    Screen.Upgrade,
+    CakeScreen,
+    MenuScreen,
+    FadeScreen,
+    KitchenScreen,
+    IngredientScreen,
+    UpgradeScreen,
+    SaveScreen
 )
 
-val allScreensMap = allScreens.associateBy { Json.encodeToJsonElement(it).jsonObject["type"]?.jsonPrimitive?.content }
+val allScreensMap = allScreens.associateBy { Json.encodeToJsonElement(it as Screen).jsonObject["type"]?.jsonPrimitive?.content }
