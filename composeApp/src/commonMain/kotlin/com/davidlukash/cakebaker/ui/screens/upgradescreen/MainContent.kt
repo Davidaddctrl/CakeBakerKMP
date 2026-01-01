d790d782df383f3a5d505da042ece8faa795a77e
david <davidlukash@hotmail.com>
2026-01-01 20:33:23 +0000

    Made upgrades scrollable

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/screens/upgradescreen/MainContent.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/screens/upgradescreen/MainContent.kt
@@ -1,19 +1,24 @@
 package com.davidlukash.cakebaker.ui.screens.upgradescreen
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.davidlukash.cakebaker.ui.BuyableItemDisplay
+import com.davidlukash.cakebaker.horizontalRowScroll
+import com.davidlukash.cakebaker.ui.HorizontalScrollBar
 import com.davidlukash.cakebaker.viewmodel.LocalMainViewModel
 
 @Composable
@@ -22,15 +27,24 @@ fun MainContent(innerPadding: PaddingValues, currentPage: String) {
     val dataViewModel = mainViewModel.dataViewModel
     val upgrades by dataViewModel.upgradesFlow.collectAsState()
     val filteredUpgrades = upgrades.filter { it.pageName == currentPage }
-    Row(
-        modifier = Modifier.padding(innerPadding).fillMaxSize(),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically,
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier.fillMaxSize().padding(innerPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        filteredUpgrades.forEach { upgrade ->
-            key(upgrade.name) {
-                UpgradeDisplay(upgrade)
+        Row(
+            modifier = Modifier.horizontalScroll(scrollState)
+                .horizontalRowScroll(coroutineScope, scrollState).fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterHorizontally),
+        ) {
+            filteredUpgrades.forEach { upgrade ->
+                key(upgrade.name) {
+                    UpgradeDisplay(upgrade)
+                }
             }
         }
+        HorizontalScrollBar(scrollState, coroutineScope)
     }
 }
