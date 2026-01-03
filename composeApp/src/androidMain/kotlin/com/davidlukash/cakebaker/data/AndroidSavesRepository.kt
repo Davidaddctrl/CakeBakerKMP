4c10e0b3265aa84c325cc546d4f92a2e63b8963b
david <davidlukash@hotmail.com>
2026-01-03 16:03:39 +0000

    Added AndroidSavesRepository

---
+++ composeApp/src/androidMain/kotlin/com/davidlukash/cakebaker/data/AndroidSavesRepository.kt
@@ -0,0 +1,40 @@
+package com.davidlukash.cakebaker.data
+
+import com.davidlukash.cakebaker.json
+import java.io.File
+
+class AndroidSavesRepository(
+    baseDirectory: File,
+) : SavesRepository() {
+    val saveDirectory = baseDirectory.resolve("saves").also { it.mkdirs() }
+
+
+    override fun listSaves(): List<SaveFile> {
+        return saveDirectory.listFiles { it.isFile }?.map {
+            val text = it.readText()
+            json.decodeFromString<SaveFile>(text)
+        } ?: emptyList()
+    }
+
+    override fun deleteSave(name: String): Boolean {
+        val saveFile = saveDirectory.resolve(name)
+        if (!saveFile.exists()) return false
+        saveFile.delete()
+        return true
+    }
+
+    override fun upsertSave(file: SaveFile): Boolean {
+        val saveFile = saveDirectory.resolve(file.name)
+        val existsBefore = saveFile.createNewFile()
+        saveFile.writeText(json.encodeToString(file))
+        return existsBefore
+    }
+
+    override fun exportSave(file: SaveFile): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun importSave(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
