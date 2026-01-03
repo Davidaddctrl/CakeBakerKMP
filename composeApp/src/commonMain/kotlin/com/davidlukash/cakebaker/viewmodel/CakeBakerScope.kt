5d06cab6649a0576e95dc1920acf6b7677d70336
david <davidlukash@hotmail.com>
2026-01-03 10:42:16 +0000

    Refactored Cake Baker scope

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/CakeBakerScope.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/CakeBakerScope.kt
@@ -13,110 +13,29 @@ class CakeBakerScope(
 ) : Scope(scopeType) {
     val localVariables = mutableMapOf<String, Object>()
 
-    override fun listVariables(): List<VariableDescriptor> {
-        return localVariables.keys.map { key ->
-            VariableDescriptor(
-                name = "locals.$key",
-                set = { localVariables[key] = it },
-            ) { localVariables[key]!! }
-        } + if (scopeType == ScopeType(EnumScopeType.GLOBAL)) {
-            dataViewModel.upgradesFlow.value.flatMap { upgrade ->
-                val directDescriptors = upgrade.toObject().asDictionary()!!.flatMap { (key, value) ->
-                    val keyString = key.asString()!!
-                    listOf(
-                        VariableDescriptor(
-                            name = "globals.upgrades.${upgrade.name}.$keyString",
-                            expectedType = when (keyString) {
-                                "pageName" -> ObjectType.STRING
-                                "imageName" -> ObjectType.STRING
-                                "name" -> ObjectType.STRING
-                                "price" -> ObjectType.NUMBER
-                                "cakeTier" -> ObjectType.NUMBER
-                                "maxLevel" -> ObjectType.NUMBER
-                                "onBuy" -> ObjectType.LIST
-                                "level" -> ObjectType.NUMBER
-                                "parameters" -> ObjectType.DICTIONARY
-                                else -> null
-                            },
-                            expectedTypeNullable = keyString == "maxLevel",
-                            set = {
-                                dataViewModel.updateUpgrade(
-                                    upgrade.mergeWith(
-                                        createObject(
-                                            mapOf(
-                                                key to it
-                                            )
-                                        )
-                                    )
-                                )
-                            }
-                        ) { value }
-                    ) + if (keyString == "parameters") {
-                        value.asDictionary()!!.map { (paramKey, paramValue) ->
-                            val paramKeyString = paramKey.asString()!!
-                            VariableDescriptor(
-                                name = "globals.upgrades.${upgrade.name}.parameters.$paramKeyString",
-                                set = { paramNewValue ->
-                                    dataViewModel.updateUpgrade(
-                                        upgrade.copy(
-                                            parameters = upgrade.parameters + mapOf(
-                                                paramKeyString to paramNewValue
-                                            )
-                                        )
-                                    )
-                                },
-                            ) { paramValue }
-                        }
-                    } else listOf()
-                }
+    fun generateUpgradeDescriptors(): List<VariableDescriptor> {
+        return dataViewModel.upgradesFlow.value.flatMap { upgrade ->
+            val directDescriptors = upgrade.toObject().asDictionary()!!.flatMap { (key, value) ->
+                val keyString = key.asString()!!
                 listOf(
                     VariableDescriptor(
-                        name = "globals.upgrades.${upgrade.name}",
-                        expectedType = ObjectType.DICTIONARY,
-                        expectedTypeNullable = false,
+                        name = "globals.upgrades.${upgrade.name}.$keyString",
+                        expectedType = when (keyString) {
+                            "pageName" -> ObjectType.STRING
+                            "imageName" -> ObjectType.STRING
+                            "name" -> ObjectType.STRING
+                            "price" -> ObjectType.NUMBER
+                            "cakeTier" -> ObjectType.NUMBER
+                            "maxLevel" -> ObjectType.NUMBER
+                            "onBuy" -> ObjectType.LIST
+                            "level" -> ObjectType.NUMBER
+                            "parameters" -> ObjectType.DICTIONARY
+                            else -> null
+                        },
+                        expectedTypeNullable = keyString == "maxLevel",
                         set = {
                             dataViewModel.updateUpgrade(
-                                upgrade.mergeWith(it)
-                            )
-                        }
-                    ) {
-                        upgrade.toObject()
-                    }
-                ) + directDescriptors
-            } + dataViewModel.allItemsFlow.value.flatMap { item ->
-                val directDescriptors = item.toObject().asDictionary()!!.map { (key, value) ->
-                    val keyString = key.asString()!!
-                    VariableDescriptor(
-                        name = "globals.items.${item.name}.$keyString",
-                        expectedType = when (keyString) {
-                            "name" -> ObjectType.STRING
-                            "type" -> ObjectType.STRING
-                            "amount" -> ObjectType.NUMBER
-                            "price" -> ObjectType.NUMBER
-                            "fastPriceGrowth" -> ObjectType.BOOLEAN
-                            "total" -> ObjectType.NUMBER
-                            "increment" -> ObjectType.NUMBER
-                            "increaseSlope" -> ObjectType.NUMBER
-                            "cakePriceAccountability" -> ObjectType.DICTIONARY
-                            "cakePrices" -> ObjectType.DICTIONARY
-                            "cakeTier" -> ObjectType.NUMBER
-                            "salePrice" -> ObjectType.NUMBER
-                            else -> ObjectType.NUMBER
-                        },
-                        expectedTypeNullable = keyString in listOf(
-                            "price",
-                            "fastPriceGrowth",
-                            "total",
-                            "increment",
-                            "increaseSlope",
-                            "cakePriceAccountability",
-                            "cakePrices",
-                            "cakeTier",
-                            "salePrice"
-                        ),
-                        set = {
-                            dataViewModel.updateItem(
-                                item.mergeWith(
+                                upgrade.mergeWith(
                                     createObject(
                                         mapOf(
                                             key to it
@@ -125,51 +44,140 @@ class CakeBakerScope(
                                 )
                             )
                         }
-                    ) {
-                        value
+                    ) { value }
+                ) + if (keyString == "parameters") {
+                    value.asDictionary()!!.map { (paramKey, paramValue) ->
+                        val paramKeyString = paramKey.asString()!!
+                        VariableDescriptor(
+                            name = "globals.upgrades.${upgrade.name}.parameters.$paramKeyString",
+                            set = { paramNewValue ->
+                                dataViewModel.updateUpgrade(
+                                    upgrade.copy(
+                                        parameters = upgrade.parameters + mapOf(
+                                            paramKeyString to paramNewValue
+                                        )
+                                    )
+                                )
+                            },
+                        ) { paramValue }
                     }
-                }
-                listOf(
-                    VariableDescriptor(
-                        name = "globals.items.${item.name}",
-                        expectedType = ObjectType.DICTIONARY,
-                        expectedTypeNullable = false,
-                        set = {
-                            dataViewModel.updateItem(
-                                item.mergeWith(it)
-                            )
-                        }
-                    ) {
-                        item.toObject()
-                    }
-                ) + directDescriptors
-            } + VariableDescriptor(
-                "globals.upgrades",
-                expectedType = ObjectType.LIST,
-                expectedTypeNullable = false,
-                set = {
-                    dataViewModel.setUpgrades(
-                        it.asList()!!.map { innerUpgrade ->
-                            Upgrade.fromObject(innerUpgrade)
-                        }
-                    )
-                }
-            ) {
-                createObject(dataViewModel.upgradesFlow.value.map { it.toObject() })
-            } + VariableDescriptor(
-                "globals.items",
-                expectedType = ObjectType.LIST,
-                expectedTypeNullable = false,
-                set = {
-                    dataViewModel.setItems(
-                        it.asList()!!.map { innerItem ->
-                            Item.fromObject(innerItem)
-                        }
-                    )
-                }
-            ) {
-                createObject(dataViewModel.allItemsFlow.value.map { it.toObject() })
+                } else listOf()
             }
+            listOf(
+                VariableDescriptor(
+                    name = "globals.upgrades.${upgrade.name}",
+                    expectedType = ObjectType.DICTIONARY,
+                    expectedTypeNullable = false,
+                    set = {
+                        dataViewModel.updateUpgrade(
+                            upgrade.mergeWith(it)
+                        )
+                    }
+                ) {
+                    upgrade.toObject()
+                }
+            ) + directDescriptors
+        } + VariableDescriptor(
+            "globals.upgrades",
+            expectedType = ObjectType.LIST,
+            expectedTypeNullable = false,
+            set = {
+                dataViewModel.setUpgrades(
+                    it.asList()!!.map { innerUpgrade ->
+                        Upgrade.fromObject(innerUpgrade)
+                    }
+                )
+            }
+        ) {
+            createObject(dataViewModel.upgradesFlow.value.map { it.toObject() })
+        }
+    }
+
+    fun generateItemDescriptors(): List<VariableDescriptor> {
+        return dataViewModel.allItemsFlow.value.flatMap { item ->
+            val directDescriptors = item.toObject().asDictionary()!!.map { (key, value) ->
+                val keyString = key.asString()!!
+                VariableDescriptor(
+                    name = "globals.items.${item.name}.$keyString",
+                    expectedType = when (keyString) {
+                        "name" -> ObjectType.STRING
+                        "type" -> ObjectType.STRING
+                        "amount" -> ObjectType.NUMBER
+                        "price" -> ObjectType.NUMBER
+                        "fastPriceGrowth" -> ObjectType.BOOLEAN
+                        "total" -> ObjectType.NUMBER
+                        "increment" -> ObjectType.NUMBER
+                        "increaseSlope" -> ObjectType.NUMBER
+                        "cakePriceAccountability" -> ObjectType.DICTIONARY
+                        "cakePrices" -> ObjectType.DICTIONARY
+                        "cakeTier" -> ObjectType.NUMBER
+                        "salePrice" -> ObjectType.NUMBER
+                        else -> ObjectType.NUMBER
+                    },
+                    expectedTypeNullable = keyString in listOf(
+                        "price",
+                        "fastPriceGrowth",
+                        "total",
+                        "increment",
+                        "increaseSlope",
+                        "cakePriceAccountability",
+                        "cakePrices",
+                        "cakeTier",
+                        "salePrice"
+                    ),
+                    set = {
+                        dataViewModel.updateItem(
+                            item.mergeWith(
+                                createObject(
+                                    mapOf(
+                                        key to it
+                                    )
+                                )
+                            )
+                        )
+                    }
+                ) {
+                    value
+                }
+            }
+            listOf(
+                VariableDescriptor(
+                    name = "globals.items.${item.name}",
+                    expectedType = ObjectType.DICTIONARY,
+                    expectedTypeNullable = false,
+                    set = {
+                        dataViewModel.updateItem(
+                            item.mergeWith(it)
+                        )
+                    }
+                ) {
+                    item.toObject()
+                }
+            ) + directDescriptors
+        } + VariableDescriptor(
+            "globals.items",
+            expectedType = ObjectType.LIST,
+            expectedTypeNullable = false,
+            set = {
+                dataViewModel.setItems(
+                    it.asList()!!.map { innerItem ->
+                        Item.fromObject(innerItem)
+                    }
+                )
+            }
+        ) {
+            createObject(dataViewModel.allItemsFlow.value.map { it.toObject() })
+        }
+    }
+
+    override fun listVariables(): List<VariableDescriptor> {
+        return localVariables.keys.map { key ->
+            VariableDescriptor(
+                name = "locals.$key",
+                set = { localVariables[key] = it },
+            ) { localVariables[key]!! }
+        } + if (scopeType == ScopeType(EnumScopeType.GLOBAL)) {
+            generateUpgradeDescriptors() + generateItemDescriptors()
         } else listOf()
     }
 
