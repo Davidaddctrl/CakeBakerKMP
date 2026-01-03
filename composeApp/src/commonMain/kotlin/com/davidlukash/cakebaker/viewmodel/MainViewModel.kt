53b997aedc3fe90d9452e76764d9ebbf78c3560d
david <davidlukash@hotmail.com>
2026-01-03 10:14:38 +0000

    Added a welcome message and function dump upon engine load to logs

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/MainViewModel.kt
@@ -2,12 +2,22 @@ package com.davidlukash.cakebaker.viewmodel
 
 import androidx.compose.runtime.compositionLocalOf
 import androidx.lifecycle.ViewModel
+import com.davidlukash.cakebaker.data.Log
+import com.davidlukash.cakebaker.data.LogType
+import com.davidlukash.cakebaker.dumpFunctionsToFile
+import kotlin.uuid.ExperimentalUuidApi
 
 class MainViewModel : ViewModel() {
     val engine = CakeBakerEngine(this).also {
         it.registerStandardFunctions()
+        dumpFunctionsToFile(it)
     }
-    val uiViewModel = UIViewModel()
+    @OptIn(ExperimentalUuidApi::class)
+    val uiViewModel = UIViewModel().also {
+        val functionDump = engine.getAllFunctions().joinToString("\n\n") { engine.describeFunction(it) }
+        it.appendLog(Log("Welcome to JsonMath 1.0.8", LogType.MESSAGE))
+        it.appendLog(Log("List of all available functions:\n$functionDump", LogType.MESSAGE))
+    }
     val themeViewModel = ThemeViewModel()
     val dataViewModel = DataViewModel(uiViewModel, engine)
 
