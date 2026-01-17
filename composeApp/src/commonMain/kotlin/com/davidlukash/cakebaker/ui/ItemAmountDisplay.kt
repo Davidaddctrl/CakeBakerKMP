17bfbe5bc2c468748ac4c2b254fb4ddadc0e7e19
david <davidlukash@hotmail.com>
2026-01-17 09:09:57 +0000

    Made ItemAmountDisplay use local content color

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/ItemAmountDisplay.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/ItemAmountDisplay.kt
@@ -1,19 +1,24 @@
 package com.davidlukash.cakebaker.ui
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.davidlukash.cakebaker.data.Item
 import com.davidlukash.cakebaker.data.ItemType
 import com.davidlukash.cakebaker.data.theme.Theme
+import com.davidlukash.cakebaker.globalDecimalMode
 import com.davidlukash.cakebaker.toEngNotation
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
 import com.ionspin.kotlin.bignum.decimal.toBigDecimal
@@ -27,83 +32,87 @@ fun ItemAmountDisplay(
     modifier: Modifier = Modifier
 ) {
     val image = theme.nameToImage(item.name)
-    Column(
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Bottom),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.defaultMinSize(minHeight = 256.dp, minWidth = 128.dp)
+    val localContentColor = if (LocalInspectionMode.current) Color.White else LocalContentColor.current
+    CompositionLocalProvider(
+        LocalContentColor provides localContentColor
     ) {
-        Text(
-            item.name.replace(" ", "\n"),
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            style = if (item.name.contains(" ")) theme.scaledStyles.verySmallBodyStyle else theme.scaledStyles.smallBodyStyle,
-        )
         Column(
-            modifier = Modifier.height(96.dp),
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Bottom),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
         ) {
-            ResourceImage(
-                image,
-                modifier = Modifier.height(if (item.name == "Money") 48.dp else 96.dp)
+            Text(
+                item.name.replace(" ", "\n"),
+                textAlign = TextAlign.Center,
+                style = if (item.name.contains(" ")) theme.scaledStyles.verySmallBodyStyle else theme.scaledStyles.smallBodyStyle,
+            )
+            Column(
+                modifier = Modifier.height(96.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                ResourceImage(
+                    image,
+                    modifier = Modifier.height(if (item.name == "Money") 48.dp else 96.dp)
+                )
+            }
+            Text(
+                "Amount",
+                textAlign = TextAlign.Center,
+                style = theme.scaledStyles.smallBodyStyle,
+            )
+            Text(
+                toEngNotation(item.amount.add(quantityChange, globalDecimalMode)),
+                color = if (quantityChange == BigDecimal.ZERO) Color.White else
+                    if (quantityChange > BigDecimal.ZERO) theme.successColor else theme.dangerColor,
+                textAlign = TextAlign.Center,
+                style = theme.scaledStyles.smallBodyStyle,
+            )
+            Text(
+                (if (quantityChange != BigDecimal.ZERO) {
+                    (if (quantityChange > BigDecimal.ZERO) "+" else "-") + toEngNotation(quantityChange.abs())
+                } else "") + " ",
+                color = if (quantityChange == BigDecimal.ZERO) Color.White else
+                    if (quantityChange > BigDecimal.ZERO) theme.successColor else theme.dangerColor,
+                textAlign = TextAlign.Start,
+                modifier = Modifier.align(Alignment.Start),
+                style = theme.scaledStyles.verySmallBodyStyle,
             )
         }
-        Text(
-            "Amount",
-            color = Color.White,
-            textAlign = TextAlign.Center,
-            style = theme.scaledStyles.smallBodyStyle,
-        )
-        Text(
-            toEngNotation(item.amount + quantityChange),
-            color = if (quantityChange == BigDecimal.ZERO) Color.White else
-                if (quantityChange > BigDecimal.ZERO) theme.successColor else theme.dangerColor,
-            textAlign = TextAlign.Center,
-            style = theme.scaledStyles.smallBodyStyle,
-        )
-
-        Text(
-            (if (quantityChange != BigDecimal.ZERO) {
-                (if (quantityChange > BigDecimal.ZERO) "+" else "-") + toEngNotation(quantityChange.abs())
-            } else "") + " ",
-            color = if (quantityChange == BigDecimal.ZERO) Color.White else
-                if (quantityChange > BigDecimal.ZERO) theme.successColor else theme.dangerColor,
-            textAlign = TextAlign.Start,
-            modifier = Modifier.align(Alignment.Start),
-            style = theme.scaledStyles.verySmallBodyStyle,
-        )
     }
 }
 
-@Preview
+const val backgroundColor = 0xFF0078FFL
+
+@Preview(showBackground = true, backgroundColor = backgroundColor)
 @Composable
 fun ItemAmountDisplayPreview() {
     val theme = Theme.default
     val item = Item(
-        name = "Money",
+        name = "Butter",
         type = ItemType.CURRENCY,
         amount = 10000.toBigDecimal(),
     )
     ItemAmountDisplay(theme = theme, item = item)
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = backgroundColor)
 @Composable
 fun ItemAmountDisplayPositiveQuantityPreview() {
     val theme = Theme.default
     val item = Item(
-        name = "Money",
+        name = "Butter",
         type = ItemType.CURRENCY,
         amount = 10000.toBigDecimal(),
     )
     ItemAmountDisplay(theme = theme, item = item, quantityChange = 100.toBigDecimal())
 }
 
-@Preview
+@Preview(showBackground = true, backgroundColor = backgroundColor)
 @Composable
 fun ItemAmountDisplayNegativeQuantityPreview() {
     val theme = Theme.default
     val item = Item(
-        name = "Money",
+        name = "Butter",
         type = ItemType.CURRENCY,
         amount = 10000.toBigDecimal(),
     )
