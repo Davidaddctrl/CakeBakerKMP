0c0acd7957ae63c1810e70e66cb5cf57f49e4295
david <davidlukash@hotmail.com>
2026-02-08 12:43:07 +0000

    Added Migration Interface

---
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/data/Migration.kt
@@ -0,0 +1,21 @@
+package com.davidlukash.cakebaker.data
+
+import com.davidlukash.cakebaker.data.save.Save
+
+interface Migration {
+    val from: Int
+    val to: Int
+
+    fun migrate(save: Save)
+
+    companion object {
+        val migration0to1 = object : Migration {
+            override val from: Int = 0
+            override val to: Int = 1
+
+            override fun migrate(save: Save) {
+
+            }
+        }
+    }
+}
