9f6d2578fc0da2d6625bb737782991f38c71af34
david <davidlukash@hotmail.com>
2026-02-14 12:02:10 +0000

    Decoupled SaveFileViewModel from UIViewModel

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/SaveFileViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/viewmodel/SaveFileViewModel.kt
@@ -2,22 +2,18 @@ package com.davidlukash.cakebaker.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.davidlukash.cakebaker.data.UIActions
 import com.davidlukash.cakebaker.data.save.Save
 import com.davidlukash.cakebaker.data.save.SaveFile
 import com.davidlukash.cakebaker.repository.ResultSavesRepositoryWrapper
 import com.davidlukash.cakebaker.repository.SavesRepository
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class SaveFileViewModel(
-    val uiViewModel: UIViewModel,
-    private val _savesRepository: SavesRepository,
+    val uiActions: UIActions,
+    _savesRepository: SavesRepository,
 ) : ViewModel() {
     private val savesRepository = ResultSavesRepositoryWrapper(_savesRepository)
     private val _saves = MutableStateFlow(emptyList<SaveFile>())
