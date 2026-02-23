2bfe25ccc5fd5b5a85c947a01ef91b918f50b2e0
david <davidlukash@hotmail.com>
2026-02-23 18:16:57 +0000

    Added ImportFromURL state to ThemeDialogState

--- composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/screens/themescreen/ThemeDialogState.kt
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/ui/screens/themescreen/ThemeDialogState.kt
@@ -3,4 +3,5 @@ package com.davidlukash.cakebaker.ui.screens.themescreen
 interface ThemeDialogState {
     data object None : ThemeDialogState
     data class Delete(val theme: String) : ThemeDialogState
+    data object ImportFromURL : ThemeDialogState
 }
