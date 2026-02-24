c5d42f7a52985f374c367e7b3b58355a7d553831
david <davidlukash@hotmail.com>
2026-02-24 23:07:01 +0000

    Added ProgressBarValues class

---
+++ composeApp/src/commonMain/kotlin/com/davidlukash/cakebaker/data/theme/ProgressBarValues.kt
@@ -0,0 +1,10 @@
+package com.davidlukash.cakebaker.data.theme
+
+import androidx.compose.ui.unit.Dp
+
+data class ProgressBarValues(
+    val elevation: Dp,
+    val borderRadius: Dp,
+    val borderWidth: Dp,
+    val minHeight: Dp
+)
